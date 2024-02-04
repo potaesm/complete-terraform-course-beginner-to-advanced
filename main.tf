6e0de857a7481edcc525a35973dd1075536c2729
potaesm <suthinan.musitmani@gmail.com>
2024-02-04 17:22:28 +0700

    add feature/modules

--- main.tf
+++ main.tf
@@ -1,28 +1,31 @@
-provider "aws" {}
-
-variable cidr_blocks {
-    description = "cidr blocks and name tags for vpc and subnets"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
+provider "aws" {
+    region = "eu-west-3"
 }
-
-variable avail_zone {}
 
 resource "aws_vpc" "myapp-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name: var.cidr_blocks[0].name
+        Name = "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
     vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name: var.cidr_blocks[1].name
-    }
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
+module "myapp-server" {
+    source = "./modules/webserver"
+    vpc_id = aws_vpc.myapp-vpc.id
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    public_key_location = var.public_key_location
+    instance_type = var.instance_type
+    subnet_id = module.myapp-subnet.subnet.id
+    avail_zone = var.avail_zone
+}
